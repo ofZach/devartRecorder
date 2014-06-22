d83035f436c8b23b63f2e131e945bf86cd5d8fb4
zach lieberman <zach@openframeworks.cc>
2014-06-22 16:06:51 -0400

    better random time...

--- chromaStatus/src/testApp.cpp
+++ chromaStatus/src/testApp.cpp
@@ -127,6 +127,12 @@ ofPoint projection( float latitude, float longitude, ofRectangle mapTo){
 //--------------------------------------------------------------
 void testApp::setup(){
     
+    
+    struct timeval time;
+    gettimeofday(&time,NULL);
+    srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
+    
+    
     parseRadioStations();
     
     loadSettings();
